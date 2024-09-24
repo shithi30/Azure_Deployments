f4f2be3d9ada506ff578830a2f36d93c77d0b308
Shithi Maitra <shithi30@gmail.com>
2024-09-24 13:06:22 -0400

    added slot notifier

--- function_app.py
+++ function_app.py
@@ -5,18 +5,22 @@ import logging
 from google.cloud import bigquery
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from bs4 import BeautifulSoup
+import pandas as pd
 import smtplib
 from email.mime.text import MIMEText
 from pretty_html_table import build_table
-import pandas as pd
 import random
+from datetime import datetime, timedelta
 import time
 import os
 import json
-# import - selenium
+# import - Selenium
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 # Function App obj.
 app = func.FunctionApp()
@@ -25,7 +29,6 @@ app = func.FunctionApp()
 @app.timer_trigger(schedule = "0 40 14 * * *", arg_name = "myTimer", run_on_startup = False, use_monitor = True) 
 def flyer_notifier(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due: logging.info("The timer is past due!")
-
     ## logic starts
 
     # credentials
@@ -103,13 +106,12 @@ def flyer_notifier(myTimer: func.TimerRequest) -> None:
         server.sendmail(sender_email, recivr_email, html_msg.as_string())
 
     ## logic ends
-    logging.info("update - Python timer trigger function executed.")
+    logging.info("Python timer trigger function executed.")
     
 # job template - 02
-@app.timer_trigger(schedule = "0 */30 * * * *", arg_name = "myTimer", run_on_startup = False, use_monitor = True)
+@app.timer_trigger(schedule = "0 0 0 31 2 *", arg_name = "myTimer", run_on_startup = False, use_monitor = True) # never trigger
 def cluster_activator(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due: logging.info("The timer is past due!")
-
     ## logic starts
 
     # pref.
@@ -134,6 +136,108 @@ def cluster_activator(myTimer: func.TimerRequest) -> None:
 
     # close window
     driver.close()
+
+    ## logic ends
+    logging.info("Python timer trigger function executed.")
+
+@app.timer_trigger(schedule = "0 */6 * * * *", arg_name = "myTimer", run_on_startup = False, use_monitor = True)
+def visa_appt_checker(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due: logging.info("The timer is past due!")
+    ## logic starts
+
+    # pref.
+    chrome_options = Options()
+    chrome_options.add_argument("headless")
+    chrome_options.add_argument("no-sandbox")
+    chrome_options.add_argument("disable-dev-shm-usage")
+    chrome_options.add_argument("user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'")
+
+    # window
+    driver = webdriver.Chrome(options = chrome_options)
+    driver.implicitly_wait(3)
+    achains = ActionChains(driver)
+
+    # url
+    driver.maximize_window()
+    driver.get("https://ais.usvisa-info.com/en-ca/niv/users/sign_in")
+
+    # user
+    elem = driver.find_element(By.ID, "user_email")
+    elem.send_keys(os.getenv("PORTAL_USER"))
+
+    # pass
+    elem = driver.find_element(By.ID, "user_password")
+    elem.send_keys(os.getenv("PORTAL_PASS"))
+
+    # terms
+    elem = driver.find_element(By.ID, "policy_confirmed")
+    achains.move_to_element(elem).click().perform()
+
+    # submit
+    elem = driver.find_element(By.NAME, "commit")
+    achains.move_to_element(elem).click().perform()
+
+    # continue
+    elem = driver.find_element(By.XPATH, ".//a[@class='button primary small']")
+    elem.click()
+
+    # reschedule 1
+    elem = driver.find_element(By.LINK_TEXT, "Reschedule Appointment")
+    elem.click()
+
+    # reschedule 2
+    time.sleep(3)
+    elems = driver.find_elements(By.LINK_TEXT, "Reschedule Appointment")
+    elems[1].click()
+
+    # acc.
+    date_df = pd.DataFrame(columns = ["consulate", "closest_date", "report_time"])
+    consulates = ["Calgary", "Halifax", "Montreal", "Ottawa", "Quebec City", "Toronto", "Vancouver"]
+
+    # consulate
+    for consulate in consulates: 
+        elem = Select(driver.find_element(By.NAME, "appointments[consulate_appointment][facility_id]"))
+        elem.select_by_visible_text(consulate)
+        time.sleep(3)
+        
+        # calendar
+        elem = driver.find_element(By.NAME, "appointments[consulate_appointment][date]")
+        try: elem.click()
+        except: continue
+        
+        # soup
+        for i in range(0, 24):
+            soup = BeautifulSoup(driver.page_source, "html.parser").find_all("td", attrs = {"data-handler": "selectDay"})
+            # availability
+            available_on_month = len(soup)
+            if available_on_month > 0: break
+            # next month
+            elem = driver.find_element(By.XPATH, ".//a[@title='Next']")
+            elem.click()
+
+        # exit calendar
+        elem = driver.find_element(By.ID, "appointments_consulate_address")
+        elem.click()
+        time.sleep(3)
+        
+        # record
+        booked_date = closest_date = "2025-12-08"
+        if available_on_month > 0: closest_date = datetime.strptime(soup[0]["data-year"] + "-" + str(int(soup[0]["data-month"]) + 1) + "-" + soup[0].get_text(), "%Y-%m-%d").strftime("%Y-%m-%d")
+        if closest_date < booked_date: date_df = pd.concat([date_df, pd.DataFrame([[consulate, closest_date, (datetime.now() - timedelta(hours = 4)).strftime("%Y-%m-%d %I:%M %p")]], columns = date_df.columns)], ignore_index = True)
+
+    # email - from, to, body
+    sender_email = "shithi30@gmail.com"
+    receiver_email = ["maitra.shithi.aust.cse@gmail.com", "shithi30@outlook.com", "Purnabchowdhury@gmail.com"]
+    body = '''Please find the earliest posted empty slots by consulates.''' + build_table(date_df, "green_dark", font_size = "12px", text_align = "left") + '''Thanks,<br>Shithi Maitra<br>Ex Asst. Manager, CS Analytics<br>Unilever BD Ltd.<br>'''
+
+    # email - object
+    html_msg = MIMEText(body, "html")
+    html_msg["Subject"], html_msg["From"], html_msg["To"] = "US Visa Appt.", "Shithi Maitra", ""
+
+    # email - send
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(sender_email, os.getenv("EMAIL_PASS"))
+        if date_df.shape[0] > 0: server.sendmail(sender_email, receiver_email, html_msg.as_string())
 
     ## logic ends
     logging.info("Python timer trigger function executed.")
